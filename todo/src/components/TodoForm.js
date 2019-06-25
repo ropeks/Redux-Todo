2729a34c758e306094e3886168392bf4aac562f5
ropeks <ropeksmilioner@gmail.com>
2019-06-25 14:40:44 +0200

    flesh out components

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TodoForm extends React.Component {
+    render() {
+        return (
+            <div>
+                todo form
+            </div>
+        )
+    }
+}
